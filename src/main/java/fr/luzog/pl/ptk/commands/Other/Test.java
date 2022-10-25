f2583984427293eae138843a29dd8645dc273353
SABIK Yanis <63198599+Luzog78@users.noreply.github.com>
2022-10-25 12:49:00 +0200

    a1.4 - Role Principle

--- src/main/java/fr/luzog/pl/ptk/commands/Other/Test.java
+++ src/main/java/fr/luzog/pl/ptk/commands/Other/Test.java
@@ -1,11 +1,16 @@
 package fr.luzog.pl.ptk.commands.Other;
 
+import fr.luzog.pl.ptk.game.Role;
 import fr.luzog.pl.ptk.utils.CmdUtils;
+import fr.luzog.pl.ptk.utils.Items;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.material.MaterialData;
 import org.bukkit.scoreboard.DisplaySlot;
 import org.bukkit.scoreboard.Objective;
 import org.bukkit.scoreboard.Scoreboard;
@@ -15,8 +20,13 @@ import java.util.List;
 
 public class Test implements CommandExecutor, TabCompleter {
 
+    boolean b = false;
+
     public boolean onCommand(CommandSender sender, Command cmd, String msg, String[] args) {
         CmdUtils u = new CmdUtils(sender, cmd, msg, args, "/test");
+
+        (b ? Role.WITCH.getKit() : Role.WIZARD.getKit()).forEach(i -> u.getPlayer().getInventory().addItem(i.build()));
+        b = !b;
 
         u.succ("Test!");
 
